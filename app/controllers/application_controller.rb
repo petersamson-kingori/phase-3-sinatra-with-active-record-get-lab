d14ca85eb80d21b96be8f38dc85b9208b9f5c40a
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-04-15 16:14:03 +0300

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < Sinatra::Base
+  # Set default content type for responses to application/json
+  before do
+    content_type 'application/json'
+  end
 
-  # add routes
+  # Route to get all bakeries as JSON array
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 
+  # Route to get a single bakery with nested baked goods by id as JSON
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    # Include baked goods association as nested array in the JSON
+    bakery.to_json(include: :baked_goods)
+  end
+
+  # Route to get baked goods sorted by price in descending order as JSON
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  # Route to get the most expensive baked good as JSON
+  get '/baked_goods/most_expensive' do
+    most_expensive_baked_good = BakedGood.order(price: :desc).first
+    most_expensive_baked_good.to_json
+  end
 end
